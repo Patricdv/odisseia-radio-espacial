186dc1d48afc8de04337f7f9ef40072f9442b0ec
Patricdv <patric.venturini@beatscode.com>
2021-07-14 17:08:28 +0100

    fixed import

--- pages/season-1/index.js
+++ pages/season-1/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 import Mic from "../../components/Mic";
 import Roberto from "../../components/Roberto";
 import Cassete from "../../components/Cassete";
